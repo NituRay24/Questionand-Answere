d344f8c80f06fd0f38028b43bd52e6eae2ec40ab
NituRay24 <raynitu24@gmail.com>
2022-07-02 14:09:59 +0530

    QUestion and Answer

---
+++ src/InputOutputOpertaions.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+public class InputOutputOpertaions {
+    public boolean checkFileExixtence()throws IOException{
+        File file= new File("java.txt");
+        System.out.println(file.exists());
+        boolean newFileCreated = file.createNewFile();
+
+        System.out.println(file.exists());
+        return file.exists();
+    }
+    public long getFileLength()throws IOException {
+        File file= new File("java1.txt");
+        file.createNewFile();
+        long fileLength = file.length();
+        return fileLength;
+    }
+    public int getFileAdd()throws IOException{
+        File file= new File("java2.txt");
+        file.createNewFile();
+        long fileAdd = file.length();
+        return (int) fileAdd;
+
+    }
+    public static void main(String[] args) {
+        InputOutputOpertaions fbo=new InputOutputOpertaions();
+        try {
+            System.out.println(fbo.getFileLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
